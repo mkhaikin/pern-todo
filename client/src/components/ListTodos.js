06752af6df959dfda141ecd485f15686374a9981
Michael <michael.khaikin@gmail.com>
2021-01-22 17:16:29 -0500

    fetching todos from using listTodos component

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React , {Fragment, useEffect, useState } from 'react';
 
-const listTodos = () => {
+const ListTodos = () => {
+    
+    const [todos, setTodos] = useState([]);
+
+    const getTodos = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/todos');
+            const jsonData = await response.json();
+            console.log(jsonData);
+            setTodos(jsonData);
+        } catch (err) {
+            console.error(err.message)
+        }
+    };
+
+    useEffect(() => {
+        getTodos()
+    },[]);
+
+    console.log(todos)
+
     return (
-        <h1>List Todos</h1>
+        <Fragment>
+            {' '}
+            <table class="table mt-5 text-center">
+                <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+                </thead>
+                <tbody>
+                {/* <tr>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>john@example.com</td>
+                    </tr> 
+                */}
+                
+                </tbody>
+            </table>   
+        </Fragment>
     )
 };
 
-export default listTodos;
+export default ListTodos;
