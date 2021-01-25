6e3277e46af89fa7886f3c6f7330479a4d747084
Michael <michael.khaikin@gmail.com>
2021-01-24 22:19:47 -0500

    crud app fully functional

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,5 +1,5 @@
 import React , {Fragment, useEffect, useState } from 'react';
-import EditTodo from './EditTodos'
+import EditTodo from './EditTodo';
 
 const ListTodos = () => {
     
@@ -7,12 +7,12 @@ const ListTodos = () => {
 
     // Delete todo function
 
-    const deleteTdo = async (id) => {
+    const deleteTodo = async (id) => {
         try {
             const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
                 method:'DELETE'
             });
-            console.log(deleteTodo);
+            // console.log(deleteTodo);
             setTodos(todos.filter(todo => todo.todo_id !== id))
         } catch (err) {
             console.error(err.message)
@@ -23,7 +23,7 @@ const ListTodos = () => {
         try {
             const response = await fetch('http://localhost:5000/todos');
             const jsonData = await response.json();
-            console.log(jsonData);
+            // console.log(jsonData);
             setTodos(jsonData);
         } catch (err) {
             console.error(err.message)
@@ -33,8 +33,6 @@ const ListTodos = () => {
     useEffect(() => {
         getTodos()
     },[]);
-
-    console.log(todos)
 
     return (
         <Fragment>
@@ -58,12 +56,14 @@ const ListTodos = () => {
                     <tr key={todo.todo_id}>
                         <td>{todo.description}</td>
                         <td>
-                            <EditTodo todo={todo}/>
+                            <EditTodo todo={todo} />
                         </td>
                         <td>
                             <button 
                                 className="btn btn-danger" 
-                                onClick={() => deleteTdo(todo.todo_id)} 
+                                onClick={() => {
+                                    deleteTodo(todo.todo_id)
+                                } } 
                             >
                                 Delete
                             </button>
